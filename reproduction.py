99f507b91ff9184fddc2fb0bac3c70482b44d824
ronny rochwerg <rochwergronny@gmail.com>
2023-06-08 23:05:33 -0400

    fixed tourney selection
    changed tourney selection to return two winners and two losers

--- reproduction.py
+++ reproduction.py
@@ -4,17 +4,19 @@ from numpy.random import default_rng
 def random_selection(pop, num_parent, rng, replacement = True):
     return rng.choice(pop, size = num_parent, replace = replacement)
 
-# tournament selection
-def tourney_selection(pop, num_parent, tourney_size, rng, replacement = True):
-    selected = []
-    # keep tourney until all parents have been selected
-    while len(selected) < num_parent:
-        #randomly select tourney individuals without replacement
-        tourney = sorted(rng.choice(len(pop), size = tourney_size, replacement=False),key=lambda x: pop[x].fitness, reverse=True)
-        #find winner based on highest fitness
-        winner = sorted(tourney, key=lambda x: pop[x].fitness, reverse=True)[0]
-        #if allowed replacement or the winner is not already selected add them to selection
-        if replacement or winner not in selected:
-            selected.append(winner)
+# tournament selection, creates two tourneys, best fitness ind mutate or recombine to replace the losers in each tourney
+# returns the indices of the winners and losers in the original population
+def tourney_selection(pop, tourney_size, rng, replacement = False):
 
-    return selected
+    #randomly select tourney individuals without replacement (for both tourneys)
+    selected = rng.choice(len(pop), size = 2*tourney_size, replacement=replacement)
+
+    # split the individuals selected into two tourneys and sort them based on fitness
+    tourney1 = sorted(selected[:tourney_size], key=lambda x: pop[x].fitness, reverse=True)
+    tourney2 = sorted(selected[tourney_size:], key=lambda x: pop[x].fitness, reverse=True)
+
+    #save winners and losers
+    winners = [tourney1[0], tourney2[0]]
+    losers = [tourney1[-1], tourney2[-1]]
+
+    return winners, losers
